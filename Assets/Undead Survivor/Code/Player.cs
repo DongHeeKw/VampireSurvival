4680b98ca9ea365f5d49fd1ff297efca6dd543d9
trdb <kth5965@gmail.com>
2025-03-03 11:06:25 +0900

    animation

---
+++ Assets/Undead Survivor/Code/Player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+
+
+
+    void FixedUpdate()
+    {
+        // 1. 힘을 준다
+        // rigid.AddForce(inputVec);
+
+        // 2. 속도 제어
+        // rigid.velocity = inputVec;
+
+        // 3. 위치 이동
+        // 물리 프레임 업데이트 주기에 맞춰 이동
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate()
+    {
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+}
